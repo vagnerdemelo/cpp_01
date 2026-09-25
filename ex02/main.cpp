72f1a45385cf6393ea4b66641ab218219d077f0c
Vagner Melo <vagnerbrmelo@gmail.com>
2026-09-25 15:44:50 -0300

    finished ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vade-mel <vade-mel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/25 15:34:00 by vade-mel          #+#    #+#             */
-/*   Updated: 2026/09/25 15:41:00 by vade-mel         ###   ########.fr       */
+/*   Updated: 2026/09/25 15:42:34 by vade-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,10 @@ int	 main(void)
 	std::string string = "HI THIS IS BRAIN";
 	std::string* stringPTR = &string;
 	std::string& stringREF = string;
+
+	std::cout << &string << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
 
 	std::cout << string << std::endl;
 	std::cout << *stringPTR << std::endl;

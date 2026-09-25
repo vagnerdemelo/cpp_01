2db59ed8c911ad6c1c3fe8d12ee39c3b2bd2473b
Vagner Melo <vagnerbrmelo@gmail.com>
2026-09-25 15:42:04 -0300

    v1 of the main.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,21 @@
 /*   By: vade-mel <vade-mel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/25 15:34:00 by vade-mel          #+#    #+#             */
-/*   Updated: 2026/09/25 15:34:01 by vade-mel         ###   ########.fr       */
+/*   Updated: 2026/09/25 15:41:00 by vade-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+
+int	 main(void)
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string* stringPTR = &string;
+	std::string& stringREF = string;
+
+	std::cout << string << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+	return (0);
+}

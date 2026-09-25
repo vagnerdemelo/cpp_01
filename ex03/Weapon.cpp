da1b208fc616de73df6c476a94ae8d647f659ae6
Vagner Melo <vagnerbrmelo@gmail.com>
2026-09-25 16:50:01 -0300

    update weapon

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: vade-mel <vade-mel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/25 15:46:38 by vade-mel          #+#    #+#             */
-/*   Updated: 2026/09/25 16:16:24 by vade-mel         ###   ########.fr       */
+/*   Updated: 2026/09/25 16:49:26 by vade-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ Weapon::Weapon(std::string type)
 
 std::string Weapon::getType() const
 {
-	return std::string();
+	return this->type;
 }
 
 void Weapon::setType(std::string type)
